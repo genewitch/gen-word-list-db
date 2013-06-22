f4ec2280ad8ae47c69c4062e6d27c22a4bc8d296
genewitch <genewitch@gmail.com>
2013-06-22 16:35:08 -0700

    Create create.sh
    yeah. 10 lines of code for that.

---
+++ create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set +x
+if [ $# -eq 0 ] 
+
+  #no key specified
+	then echo "error 1"
+
+	#key exists!
+	elif [ -e /opt/tmpfs/$1 ]
+	then echo "error 10"
+	else
+		if [ ${#2} -eq 0 ]
+			#no value, call self with "0" as the parameter
+			then ./$0 $1 "0"
+		else echo $2 > /opt/tmpfs/$1
+		fi
+fi
